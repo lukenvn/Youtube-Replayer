c22d443bc739a584960619b4be2d298f9d1ea038
luke-nhung <ngovannhung@gmail.com>
2017-04-09 09:49:27 +0700

    implemented

--- scripts/background.js
+++ scripts/background.js
@@ -3,6 +3,20 @@
  */
 
 var repeatAtB =false;
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([
+            {
+                conditions: [
+                    new chrome.declarativeContent.PageStateMatcher({
+                        pageUrl: { urlContains: 'www.youtube.com' },
+                    })
+                ],
+                actions: [ new chrome.declarativeContent.ShowPageAction() ]
+            }
+        ]);
+    });
+});
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if( request.message === "enable_auto_repeat_at_b" ) {
