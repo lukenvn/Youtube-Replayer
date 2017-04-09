55e5adf24b8d28be62263a263f049a18175901b0
luke-nhung <ngovannhung@gmail.com>
2017-04-09 10:26:25 +0700

    refactor code

--- scripts/background.js
+++ scripts/background.js
@@ -3,34 +3,38 @@
  */
 
 var repeatAtB =false;
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-        chrome.declarativeContent.onPageChanged.addRules([
-            {
-                conditions: [
-                    new chrome.declarativeContent.PageStateMatcher({
-                        pageUrl: { urlContains: 'www.youtube.com' },
-                    })
-                ],
-                actions: [ new chrome.declarativeContent.ShowPageAction() ]
-            }
-        ]);
+
+function registerListenerPageAction(){
+    chrome.runtime.onInstalled.addListener(function() {
+        chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+            chrome.declarativeContent.onPageChanged.addRules([
+                {
+                    conditions: [
+                        new chrome.declarativeContent.PageStateMatcher({
+                            pageUrl: { urlContains: 'www.youtube.com' },
+                        })
+                    ],
+                    actions: [ new chrome.declarativeContent.ShowPageAction() ]
+                }
+            ]);
+        });
     });
-});
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
+}
+function registerListenerMessage(){
+    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         if( request.message === "enable_auto_repeat_at_b" ) {
             repeatAtB = request.enable;
-
             console.log("receive request update checkAutoRepeatAtB:"+ repeatAtB);
         }
-    }
-);
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
+    });
+    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         if( request.message === "check_auto_repeat_at_b" ) {
             console.log("receive request check checkAutoRepeatAtB");
             sendResponse({enableRepeat:repeatAtB });
         }
-    }
-);
+    });
+}
+
+
+registerListenerMessage();
+registerListenerPageAction();
--- scripts/popup.js
+++ scripts/popup.js
@@ -2,42 +2,40 @@
  * Created by luke on 03/29/2017.
  */
 
-function notifyAutoRepeatWhenBSetting(){
+function notifyAutoRepeatWhenBSetting() {
 
 
 }
-var replayBCheckbox,enableRepeatAtB;
-function checkboxListener(){
+var replayBCheckbox, enableRepeatAtB;
+function checkboxListener() {
     replayBCheckbox = $('#repeatBCheckbox');
     replayBCheckbox.click(function () {
-        if (isReplayChecked()){
+        if (isReplayChecked()) {
             sendNotification(true);
-        }else{
+        } else {
             sendNotification(false);
         }
-        console.log("change :" +isReplayChecked());
+        console.log("change :" + isReplayChecked());
     });
 }
-function  enableReplayBCheckbox(value) {
+function enableReplayBCheckbox(value) {
     replayBCheckbox.prop('checked', value);
 }
 
-function  checkAutoRepeatAtB () {
-    chrome.runtime.sendMessage({message: "check_auto_repeat_at_b"}, function(response) {
+function checkAutoRepeatAtB() {
+    chrome.runtime.sendMessage({message: "check_auto_repeat_at_b"}, function (response) {
         enableReplayBCheckbox(response.enableRepeat);
     });
-
 }
-function isReplayChecked(){
-
+function isReplayChecked() {
     return replayBCheckbox.is(':checked');
 }
-function sendNotification(value){
+function sendNotification(value) {
 // This line is new!
     chrome.runtime.sendMessage({"message": "enable_auto_repeat_at_b", "enable": value});
 }
 $(function () {
-   notifyAutoRepeatWhenBSetting();
+    notifyAutoRepeatWhenBSetting();
     checkboxListener();
     checkAutoRepeatAtB();
 });
